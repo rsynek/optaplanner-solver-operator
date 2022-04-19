cd4d8a4df58b0d92383e9bde027007cddaf6a790
Radovan Synek <rsynek@redhat.com>
2022-04-19 16:07:40 +0200

    Finish the reconciler test

--- src/test/java/org/optaplanner/k8s/operator/solver/SolverReconcilerTest.java
+++ src/test/java/org/optaplanner/k8s/operator/solver/SolverReconcilerTest.java
@@ -3,24 +3,18 @@ package org.optaplanner.k8s.operator.solver;
 import static java.util.concurrent.TimeUnit.MINUTES;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
-//import static org.hamcrest.CoreMatchers.is;
-//import static org.hamcrest.MatcherAssert.assertThat;
-//import static org.hamcrest.Matchers.notNullValue;
 
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.optaplanner.k8s.operator.solver.model.Solver;
 import org.optaplanner.k8s.operator.solver.model.SolverSpec;
-import org.optaplanner.k8s.operator.solver.model.messaging.MessagingAddress;
-
-import io.fabric8.kubernetes.api.model.GenericKubernetesResource;
-import io.fabric8.kubernetes.api.model.GenericKubernetesResourceBuilder;
 import io.fabric8.kubernetes.api.model.apps.Deployment;
 import io.fabric8.kubernetes.client.server.mock.KubernetesServer;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.kubernetes.client.KubernetesTestServer;
 import io.quarkus.test.kubernetes.client.WithKubernetesTestServer;
+import org.optaplanner.k8s.operator.solver.model.messaging.MessagingAddress;
 
 @WithKubernetesTestServer
 @QuarkusTest
@@ -31,12 +25,9 @@ public class SolverReconcilerTest {
 
     @Test
     void canReconcile() {
-//        GenericKubernetesResource topicCrd = new GenericKubernetesResourceBuilder().with
-//        mockServer.getClient().genericKubernetesResources("kafka.strimzi.io/v1beta2", "KafkaTopic")
-//                .createOrReplace()
-//
         final Solver solver = new Solver();
-        solver.getMetadata().setName("test-solver");
+        final String solverName = "test-solver";
+        solver.getMetadata().setName(solverName);
         solver.setSpec(new SolverSpec());
         solver.getSpec().setSolverImage("solver-project-image");
         solver.getSpec().setKafkaBootstrapServers("kafkaServers");
@@ -51,9 +42,9 @@ public class SolverReconcilerTest {
                     .get();
             assertThat(updatedSolver.getStatus()).isNotNull();
             assertThat(updatedSolver.getStatus().getInputMessagingAddress())
-                    .isEqualTo("test-solver" + "-" + MessagingAddress.INPUT.getName());
+                    .isEqualTo(solverName + "-" + MessagingAddress.INPUT.getName());
             assertThat(updatedSolver.getStatus().getOutputMessagingAddress())
-                    .isEqualTo("test-solver" + "-" + MessagingAddress.OUTPUT.getName());
+                    .isEqualTo(solverName + "-" + MessagingAddress.OUTPUT.getName());
         });
 
         List<Deployment> deployments = mockServer.getClient()
